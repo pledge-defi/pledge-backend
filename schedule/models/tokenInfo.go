8e2569b0c576001ba98ceb6564b752029863745f
codingmasterwang <codingmaster.wang@outlook.com>
2022-03-09 15:23:39 +0800

    add token list api

--- schedule/models/tokenInfo.go
+++ schedule/models/tokenInfo.go
@@ -14,6 +14,7 @@ type TokenInfo struct {
 	Symbol       string `json:"symbol" gorm:"column:symbol"`
 	ChainId      string `json:"chain_id" gorm:"column:chain_id"`
 	Price        string `json:"price" gorm:"column:price"`
+	Decimals     int    `json:"decimals" gorm:"column:decimals"`
 	AbiFileExist int    `json:"abi_file_exist" gorm:"column:abi_file_exist"`
 	CreatedAt    string `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt    string `json:"updated_at" gorm:"column:updated_at"`
