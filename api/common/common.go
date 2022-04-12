62be9e23b83fe7b5fd963424263d48a61fa4f6ab
codingmasterwang <codingmaster.wang@outlook.com>
2022-04-12 09:28:14 +0800

    remove unused func

--- api/common/common.go
+++ api/common/common.go
@@ -1,18 +1,7 @@
 package common
 
 import (
-	"path"
 	"pledge-backend/config"
-	"runtime"
 )
 
 var Config *config.Conf
-
-func GetCurrentAbPathByCaller() string {
-	var abPath string
-	_, filename, _, ok := runtime.Caller(0)
-	if ok {
-		abPath = path.Dir(filename)
-	}
-	return abPath
-}
