2a021e2ba6db455202ef5cf0d5436860fddcad7e
codingmasterwang <codingmaster.wang@outlook.com>
2022-04-12 09:35:27 +0800

    update route

--- api/routes/route.go
+++ api/routes/route.go
@@ -10,28 +10,28 @@ import (
 func InitRoute(e *gin.Engine) *gin.Engine {
 
 	// version group
-	group := e.Group("/api/v" + config.Config.Env.Version)
+	v2Group := e.Group("/api/v" + config.Config.Env.Version)
 
 	// pledge-defi backend
 	poolController := controllers.PoolController{}
-	group.GET("/poolBaseInfo", poolController.PoolBaseInfo)                                   //pool base information
-	group.GET("/poolDataInfo", poolController.PoolDataInfo)                                   //pool data information
-	group.GET("/token", poolController.TokenList)                                             //pool token information
-	group.POST("/pool/debtTokenList", middlewares.CheckToken(), poolController.DebtTokenList) //pool debtTokenList
-	group.POST("/pool/search", middlewares.CheckToken(), poolController.Search)               //pool search
+	v2Group.GET("/poolBaseInfo", poolController.PoolBaseInfo)                                   //pool base information
+	v2Group.GET("/poolDataInfo", poolController.PoolDataInfo)                                   //pool data information
+	v2Group.GET("/token", poolController.TokenList)                                             //pool token information
+	v2Group.POST("/pool/debtTokenList", middlewares.CheckToken(), poolController.DebtTokenList) //pool debtTokenList
+	v2Group.POST("/pool/search", middlewares.CheckToken(), poolController.Search)               //pool search
 
 	// plgr-usdt price
 	priceController := controllers.PriceController{}
-	group.GET("/price", priceController.NewPrice) //new price on ku-coin-exchange
+	v2Group.GET("/price", priceController.NewPrice) //new price on ku-coin-exchange
 
 	// pledge-defi admin backend
 	multiSignPoolController := controllers.MultiSignPoolController{}
-	group.POST("/pool/setMultiSign", middlewares.CheckToken(), multiSignPoolController.SetMultiSign) //multi-sign set
-	group.POST("/pool/getMultiSign", middlewares.CheckToken(), multiSignPoolController.GetMultiSign) //multi-sign get
+	v2Group.POST("/pool/setMultiSign", middlewares.CheckToken(), multiSignPoolController.SetMultiSign) //multi-sign set
+	v2Group.POST("/pool/getMultiSign", middlewares.CheckToken(), multiSignPoolController.GetMultiSign) //multi-sign get
 
 	userController := controllers.UserController{}
-	group.POST("/user/login", userController.Login)                             // login
-	group.POST("/user/logout", middlewares.CheckToken(), userController.Logout) // logout
+	v2Group.POST("/user/login", userController.Login)                             // login
+	v2Group.POST("/user/logout", middlewares.CheckToken(), userController.Logout) // logout
 
 	return e
 }
